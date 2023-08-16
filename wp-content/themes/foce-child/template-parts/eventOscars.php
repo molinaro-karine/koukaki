48ec3b55a70f06cfcd986788ceb8adbbe264d45d
molinaro karine <molinaro.karine@hotmail.com>
2023-08-16 07:13:35 +0200

    Section oscars

---
+++ wp-content/themes/foce-child/template-parts/eventOscars.php
@@ -0,0 +1,11 @@
+
+
+
+<section class="eventOscars">
+    <div>
+        <h2 class="titreOscar">Fleurs d’oranger & chats errants<br>est nominé aux Oscars Short<br>Film Animated de 2022 !</h2>
+    </div>
+    <div class=imgOscars>
+        <img src=<?php echo get_theme_file_uri("./assets/images/oscars.png");?> alt="image oscar">
+    </div>
+    </section>
